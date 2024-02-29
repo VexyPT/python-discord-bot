760b47627a6ccf9cff7fddcbdbe4a4a54b9e5537
VexyPT <danifilipe221205@gmail.com>
2024-02-29 19:47:52 +0000

    Buttons & Select Menus

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from discord import Intents,Interaction, ButtonStyle, ui
+import discord
 from discord.ext import commands
-intents = Intents.all()
+intents = discord.Intents.all()
 from secret import TOKEN
 bot = commands.Bot(command_prefix=".", intents=intents)
 
@@ -8,15 +8,42 @@ bot = commands.Bot(command_prefix=".", intents=intents)
 @bot.command()
 async def button(ctx:commands.Context):
 
-    async def button_response(interact:Interaction):
+    async def button_response(interact:discord.Interaction):
         await interact.response.send_message('Button Pressed')
 
-
-    view = ui.View()
-    button = ui.Button(label='Button', style=ButtonStyle.success)
+    view = discord.ui.View()
+    button = discord.ui.Button(label='Button', style=discord.ButtonStyle.success)
     button.callback = button_response
 
+    buttonURL = discord.ui.Button(label='Click me please', url="https://www.youtube.com/watch?v=dQw4w9WgXcQ1")
+
     view.add_item(button)
+    view.add_item(buttonURL)
+    await ctx.reply(view=view)
+
+@bot.command()
+async def favoriteGame(ctx:commands.Context):
+
+    async def select_response(interaction:discord.Interaction):
+        choosed = interaction.data['values'][0]
+        games = {
+            '1':'Minecraft',
+            '2':'GTA V',
+            '3':'League of Legends'
+        }
+        game_choosed = games[choosed]
+        await interaction.response.send_message(f'O teu jogo favorito é {game_choosed}')
+    
+    selectMenu = discord.ui.Select(placeholder="Choose a option")
+    options = [
+        discord.SelectOption(label='Minecraft', value='1'),
+        discord.SelectOption(label='GTA V', value='2'),
+        discord.SelectOption(label='League of Legends', value='3') 
+    ]
+    selectMenu.options = options
+    selectMenu.callback = select_response
+    view = discord.ui.View()
+    view.add_item(selectMenu)
     await ctx.reply(view=view)
 
 @bot.event
